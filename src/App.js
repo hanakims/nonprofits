4eea75b973e3ec5b0150ccf376bcde0ac4173c71
Hana Kim <hsk003@ucsd.edu>
2020-10-16 11:40:57 -0700

    added org delete and incrementing boosts

--- src/App.js
+++ src/App.js
@@ -3,21 +3,44 @@ import './App.css';
 
 import OrgContentMaker from './OrgContentMaker';
 
-const listOfOrgs = ["Red Cross", "Doctors Without Borders"];
+class App extends React.Component {
+  state = {
+    currentInput: "",
+    listOfOrgs: ["Red Cross", "Doctors Without Borders"]
+  }
+  inputHandler = (event) => {
+    this.setState({currentInput: event.target.value});
+  }
+  submitHandler = () => {
+    let newCopy = this.state.listOfOrgs;
+    newCopy.push(this.state.currentInput);
+    this.setState({listOfOrgs: newCopy});
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {
-          listOfOrgs.map((x) => {
-            return <OrgContentMaker name={x} key={x} />
-          })
-        }
-      </header>
-    </div>
-  );
+  deleteOrg = (index) => {
+    let newCopy = this.state.listOfOrgs;
+    newCopy.splice(index, 1);
+    this.setState({listOfOrgs: newCopy})
+  }
+
+  render = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            add a nonprofit
+          </p>
+          <input value={this.state.currentInput} onChange={this.inputHandler}/>
+          <button onClick={this.submitHandler}>add the nonprofit</button>
+          {
+            this.state.listOfOrgs.map((x, index) => {
+              return <OrgContentMaker name={x} key={x} deleteFunc={() => {this.deleteOrg(index)}}/>
+            })
+          }
+        </header>
+      </div>
+    );
+  }
 }
-
 
 export default App;
--- src/OrgContentMaker.js
+++ src/OrgContentMaker.js
@@ -7,6 +7,17 @@ class OrgContentMaker extends React.Component {
     clickHandler = () => {
         this.setState({boosts: this.state.boosts + 1})
     }
+
+    componentDidMount = () => {
+      this.stopper = setInterval(() => {
+        this.setState((prevState) => {return {boosts: prevState.boosts + 1}});
+      } , 1000);
+    }
+
+    componentWillUnmount = () => {
+      clearInterval(this.stopper);
+    }
+
     render = () => {
       return (
         <div>
@@ -16,6 +27,7 @@ class OrgContentMaker extends React.Component {
             boosts: {this.state.boosts}
           </p>
           <button onClick={this.clickHandler}> Boost this NonProfit!</button>
+          <button onClick={this.props.deleteFunc}> Delete Nonprofit</button>
         </div>
       )
     }
