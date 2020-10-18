6299bba3f053449961b87e9e498c27f48e30bdcc
Hana Kim <hsk003@ucsd.edu>
2020-10-17 23:49:43 -0700

    changed classes to hooks and implemented database

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require('cors');
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.set("json spaces", 2);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "orgs",
+});
+
+app.get('/api/get', (req, res) => {
+    const sqlSelect = "SELECT * FROM org_posts";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    })
+});
+
+app.post('/api/insert', (req, res) => {
+
+    const org = req.body.orgs;
+    const post = req.body.post;
+
+    const sqlInsert = 
+        "INSERT INTO org_posts (org, post) VALUES (?, ?)";
+    db.query(sqlInsert, [org, post], (err, result) => {
+        console.log(result);
+    })
+});
+
+app.delete('/api/delete/:org', (req, res) => {
+    const org = req.params.org;
+    const sqlDelete = "DELETE FROM org_posts WHERE org = ?";
+    db.query(sqlDelete, org, (err, result) => {
+        if (err) console.log(err);
+    })
+});
+
+app.put('/api/update', (req, res) => {
+    const org = req.body.org;
+    const post = req.body.post
+    const sqlUpdate = "UPDATE org_posts SET post = ? WHERE org = ?";
+    db.query(sqlUpdate, [post, org], (err, result) => {
+        if (err) console.log(err);
+    })
+});
+
+app.listen(PORT, function () {
+    console.log(`server listening on port ${PORT}`)
+});
--- src/App.js
+++ src/App.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 import OrgContentMaker from './OrgContentMaker';
+
+function App() {
+  const [org, setOrg] = useState("");
+  const [post, setPost] = useState("");
+  const [postList, setPostList] = useState([])
+  const [newPost, setNewPost] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:5000/api/get").
+      then((response) => {
+        setPostList(response.data);
+      });
+  }, []);
+
+  const submitPost = () => {
+    Axios.post("http://localhost:5000/api/insert", {
+      org: org, 
+      post: post
+    }) ;
+    setPostList([
+        ...postList, 
+        {org: org, post: post},
+      ]);
+  }
+
+  const deletePost = (org) => {
+    Axios.delete(`http://localhost:5000/api/delete/${org}`);
+  }
+
+  const updatePost = (org) => {
+    Axios.put('http://localhost:5000/api/update', {
+      org: org, 
+      post: newPost
+    });
+    setNewPost("");
+  }
+
+  return (
+    <div className="App">
+        <div className="Form">
+          <label>Nonprofit:</label>
+          <input 
+            type="text" 
+            name="org" 
+            onChange={(input) => {
+              setOrg(input.target.value);
+            }}
+          />
+          <label>Post:</label>
+          <input 
+            type="text" 
+            name="post" 
+            onChange={(input) => {
+              setPost(input.target.value);
+            }}
+          />
+
+          <button onClick={submitPost}>add the nonprofit</button>
+
+          {postList.map((x) => {
+            return (
+              <div>
+                <div>Nonprofit: {x.org} | Post: {x.post}</div>
+                <button onClick={() => {(deletePost(x.org))}}>delete</button>
+                <input 
+                  type="text" 
+                  onChange={(input) => {setNewPost(input.target.value);
+                }}
+                />
+                <button onClick={() => {(updatePost(x.org))}}>update</button>
+              </div>
+            );
+          })}
+          
+        </div>
+    </div>
+  );
+}
+
+export default App;
+/*
 
 class App extends React.Component {
   state = {
@@ -44,3 +126,4 @@ class App extends React.Component {
 }
 
 export default App;
+*/
